29dcc43003546a27839e2878a3969cd6c341aed1
Gowtham Chinnaswamy <105277886+thePerfectGroove@users.noreply.github.com>
2023-03-23 08:47:06 +0530

    Add files via upload

---
+++ pgm1.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+# To use the Python Selenium Selector you have to use the By class
+from selenium.webdriver.common.by import By
+
+class chlogin:
+    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    username = 'Admin'
+    password = 'admin123'
+    # username_locator is a TagName
+    username_locator = 'username'
+    # password_locator is a TagName
+    password_locator = 'password'
+    # Submit Button Locator as XPATH
+    submitBox_locator = '//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
+
+    # Startup method for the CRM application
+    def Browsing(self):
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+
+
+    # Method for login into the CRM application
+    def login(self):
+        sleep(5)
+        self.driver.find_element(by=By.NAME, value=self.username_locator).send_keys(self.username)
+        self.driver.find_element(by=By.NAME, value=self.password_locator).send_keys(self.password)
+        self.driver.find_element(by=By.XPATH, value=self.submitBox_locator).click()
+
+
+chlogin().Browsing()
+chlogin().login()
+
